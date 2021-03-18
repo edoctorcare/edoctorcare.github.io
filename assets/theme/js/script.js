ec96c6c12905823f2a3b99b7a3d75c48624bba93
edoctorcare <61178298+edoctorcare@users.noreply.github.com>
2021-03-18 17:34:15 +0000

    Update script.js

--- assets/theme/js/script.js
+++ assets/theme/js/script.js
@@ -1110,3 +1110,17 @@
             $(e.target).parents('.form-group').find('.value')[0].innerHTML = e.target.value;
         });
     }(jQuery));
+
+const $icon = document.querySelector('.icon');
+const $arrow = document.querySelector('.arrow');
+
+$icon.onclick = () => {
+  $arrow.animate([
+    {left: '0'},
+    {left: '10px'},
+    {left: '0'}
+  ],{
+    duration: 700,
+    iterations: Infinity
+  });
+}
